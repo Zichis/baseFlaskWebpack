5e6fc86b294332d65a492adb6350c438094278af
Ezichi Ebere Ezichi <tripleee@yahoo.co.uk>
2020-08-04 20:55:29 +0100

    blog package created

---
+++ components/blog/blog.py
@@ -0,0 +1,11 @@
+from flask import Blueprint, render_template
+
+blog_bp = Blueprint(
+    'blog_bp',
+    __name__,
+    template_folder='templates'
+)
+
+@blog_bp.route('/')
+def index():
+    return render_template('blog/index.html')
