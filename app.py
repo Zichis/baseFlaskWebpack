3ee0cccfb959c850c62311a35238b4c68640f7d8
Ezichi Ebere Ezichi <tripleee@yahoo.co.uk>
2020-08-04 20:56:05 +0100

    blog blueprint registered

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+from flask_webpack.components.blog.blog import blog_bp
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
 app = Flask(__name__)
+
+app.register_blueprint(blog_bp, url_prefix="/blog")
 
 @app.route('/')
 def index():
